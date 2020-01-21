8c15c1eba085252fdc5b7d52eb7838ebdd7d5aeb
aniket kumar <kumaranik8431@gmail.com>
2020-01-21 10:19:40 +0530

    my first git web project

---
+++ webdevelopment/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>FORM</h1>
+    <form>
+        <input type="text"placeholder="Name"><br><br>
+        <input type="email"placeholder="Email"><br><br>
+        <button>
+        	<input type="submit" name="submit">
+    </button>
+    </form>
+    
+</body>
+</html>
