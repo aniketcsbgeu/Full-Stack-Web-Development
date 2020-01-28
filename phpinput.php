9407afa71339c39d98697252e01f8c32891129c9
aniket kumar <kumaranik8431@gmail.com>
2020-01-28 10:58:15 +0530

    modify data input in database

--- phpinput.php
+++ phpinput.php
@@ -3,26 +3,31 @@
 ?>
 <?php 
 if (isset($_POST['btn'])){
-	echo "dear, ".$_POST["username"]." your details are"."<br> ";
-echo $_POST["id"];
-echo $_POST["username"];
-echo $_POST["age"];
-echo $_POST["contact"];
-echo $_POST["email"];
+echo "Dear, ".$_POST["username"]." you are welcome.".'<br>';
+
+$username=$_POST["username"];
+$age= $_POST["age"];
+$contact=$_POST["contact"];
+$email=$_POST['email'];
+
+$sql="INSERT INTO users(name,age,contact,email) VALUES ('$username','$age','$contact','$email')";
+mysqli_query($conn,$sql);
 }
 else{
       echo "welcome guest"; 
 }
+
+
 ?>
 <html>
 	<head>
 		<title>input form</title>
 	</head>
 	<body>
-		<form action="phpinput.php" method="post"> 
-			ID:<input name="id",type="text" placeholder="enter id"><br>
-			name:<input type="text" name="username"><br>
-			age:<input type="text" nmae="age" placeholder="enter age"><br>
+		<form action="phpinput.php" method="post">
+		   
+			name:<input type="text" name="username" placeholder="enter name"><br>
+			age:<input type="text" name="age" placeholder="enter age"><br>
 			contact:<input type="text" name="contact" placeholder="enter age"><br>
 			email:<input type="text" name="email" placeholder="enter email"><br>
 			<input type="submit" name='btn'>
