bf73c5491dd1140e4e5bcd15be5035b02ee658c4
aniket kumar <kumaranik8431@gmail.com>
2020-03-18 19:59:06 +0530

    question 2 midsem

---
+++ Que2/que2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Parse JSON data</title>
+	<script type="text/javascript" src="json.js"></script>
+</head>
+<body>
+<p><strong><i>Id of Users Completed maximum number of task:-</i></strong></p>
+<p id="max"></p><br>
+<p><strong><i>Id of Users Completed minimum number of task:-</i></strong></p>
+<p id="min"></p>
+<script type="text/javascript" > 
+	document.getElementById("max").innerHTML = trueid;
+	document.getElementById("min").innerHTML = falseid;
+</script>
+</body>
+</html>
